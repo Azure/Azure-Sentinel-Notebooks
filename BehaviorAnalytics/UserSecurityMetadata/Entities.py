627a92b48aba0d583832324a9cbb34d2415ea8c6
Pete Bryan <peter.bryan@microsoft.com>
2019-12-04 12:57:36 +0000

    Update Entities.py

--- BehaviorAnalytics/UserSecurityMetadata/Entities.py
+++ BehaviorAnalytics/UserSecurityMetadata/Entities.py
@@ -1,4 +1,8 @@
-# Copyright (c) Microsoft Corporation. Licensed under the MIT license.
+"""
+ Copyright (c) Microsoft Corporation. All rights reserved.
+ Licensed under the MIT License. See License.txt in the project root for
+ license information.
+"""
 from validate_email import validate_email
 from Utils import executeProcess
 from NodeEdge import Node, DrawableNode
