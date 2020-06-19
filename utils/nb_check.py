c6ee68d524280d706c067702087d9ad603f1f3cf
Ian Hellen <ianhelle@microsoft.com>
2020-06-18 20:23:31 -0700

    Tidying up script and updating default versions.

--- utils/nb_check.py
+++ utils/nb_check.py
@@ -4,32 +4,28 @@
 # license information.
 # --------------------------------------------------------------------------
 """Checker for Python and msticpy versions."""
-import importlib
-import os
 import sys
-import warnings
-from IPython.display import display, HTML, Markdown
+from IPython.display import display, HTML
 
-warn_mssg = []
-err_mssg = []
+
 MISSING_PKG_ERR = """
-    <h3><font color='orange'>The package '<b>{package}</b>' is not 
+    <h3><font color='orange'>The package '<b>{package}</b>' is not
     installed or has an incorrect version</font></h3>
     <h4>Please install this now</h4>
     """
 MIN_PYTHON_VER_DEF = (3, 6)
-MSTICPY_REQ_VERSION = (0, 5, 0)
+MSTICPY_REQ_VERSION = (0, 5, 2)
 
 
 def check_python_ver(min_py_ver=MIN_PYTHON_VER_DEF):
     """
-    Checks the current version of the Python kernel.
-    
+    Check the current version of the Python kernel.
+
     Parameters
     ----------
     min_py_ver : Tuple[int, int]
         Minimum Python version
-        
+
     Raises
     ------
     RuntimeError
@@ -38,18 +34,22 @@ def check_python_ver(min_py_ver=MIN_PYTHON_VER_DEF):
     """
     display(HTML("Checking Python kernel version..."))
     if sys.version_info < min_py_ver:
-        display(HTML(
-            """
+        display(
+            HTML(
+                """
             <h3><font color='red'>This notebook requires a different notebook
             (Python) kernel version.</h3></font>
-            From the Notebook menu (above), choose <b>Kernel</b> then 
+            From the Notebook menu (above), choose <b>Kernel</b> then
             <b>Change Kernel...</b> from the menu.<br>
             Select a <b>Python %s.%s</b> (or later) version kernel and then re-run
             this cell.<br><br>
-            """ % min_py_ver
-        ))
-        display(HTML(
             """
+                % min_py_ver
+            )
+        )
+        display(
+            HTML(
+                """
             Please see the <b><a href="./TroubleShootingNotebooks.ipynb">
             TroubleShootingNotebooks</a></b>
             in this folder for more information<br><br><hr>
@@ -58,21 +58,24 @@ def check_python_ver(min_py_ver=MIN_PYTHON_VER_DEF):
         )
         raise RuntimeError("Python %s.%s or later kernel is required." % min_py_ver)
 
-    display(HTML(
-        "Python kernel version %s.%s.%s OK" % (
-            sys.version_info[0], sys.version_info[1], sys.version_info[2]
+    display(
+        HTML(
+            "Python kernel version %s.%s.%s OK"
+            % (sys.version_info[0], sys.version_info[1], sys.version_info[2])
         )
-    ))
+    )
 
+
+# pylint: disable=import-outside-toplevel
 def check_mp_ver(min_msticpy_ver=MSTICPY_REQ_VERSION):
     """
-    Checks the current version of .
-    
+    Check and optionally update the current version of msticpy.
+
     Parameters
     ----------
     min_py_ver : Tuple[int, int]
         Minimum Python version
-        
+
     Raises
     ------
     RuntimeError
@@ -82,26 +85,31 @@ def check_mp_ver(min_msticpy_ver=MSTICPY_REQ_VERSION):
     display(HTML("Checking msticpy version..."))
     try:
         import msticpy
+
         mp_version = tuple([int(v) for v in msticpy.__version__.split(".")])
         if mp_version < min_msticpy_ver:
             raise ImportError("msticpy %s.%s.%s or later is needed." % min_msticpy_ver)
-            
+
     except ImportError:
         display(HTML(MISSING_PKG_ERR.format(package="msticpy")))
-        resp = input("Install? (y/n)")
+        resp = input("Install? (y/n)")  # nosec
         if resp.casefold().startswith("y"):
             raise ImportError("Install msticpy")
-        else:
-            display(HTML(
+
+        display(
+            HTML(
                 """
-                <h3><font color='red'>The notebook cannot be run without
-                the correct version of '<b>%s</b>' (%s.%s.%s or later)
-                </font></h3>
-                Please see the <b><a href="./TroubleShootingNotebooks.ipynb">
-                TroubleShootingNotebooks</a></b>
-                in this folder for more information<br><br><hr>
-                """ % ("msticpy", *min_msticpy_ver)
-                )
+            <h3><font color='red'>The notebook cannot be run without
+            the correct version of '<b>%s</b>' (%s.%s.%s or later)
+            </font></h3>
+            Please see the <b><a href="./TroubleShootingNotebooks.ipynb">
+            TroubleShootingNotebooks</a></b>
+            in this folder for more information<br><br><hr>
+            """
+                % ("msticpy", *min_msticpy_ver)
             )
-            raise RuntimeError("msticpy %s.%s.%s or later is required." % min_msticpy_ver)
+        )
+        raise RuntimeError(
+            "msticpy %s.%s.%s or later is required." % min_msticpy_ver
+        )
     display(HTML("msticpy version %s.%s.%s OK" % mp_version))
