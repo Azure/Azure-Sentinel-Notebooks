9aa09c67fc420351b04335ff9806ad7ff3e0ccdd
Ian Hellen <ianhelle@microsoft.com>
2021-04-14 15:42:20 -0700

    Updating kernels to azureml_38
    Updating check_nb_kernel.py with AML kernel specs

--- utils/check_nb_kernel.py
+++ utils/check_nb_kernel.py
@@ -54,11 +54,16 @@ IP_KERNEL_SPEC = {
         "display_name": "Python 3.6",
     },
     "python3": {"name": "python3", "language": "python", "display_name": "Python 3"},
-    "python3-azureml": {
+    "azureml_36": {
         "name": "python3-azureml",
         "language": "python",
-        "display_name": "Python 3.6 - AzureML",
+        "display_name": "Python 3.6 - AzureML"
     },
+    "azureml_38": {
+        "name": "python38-azureml",
+        "language": "python",
+        "display_name": "Python 3.8 - AzureML"
+    }
 }
 
 
